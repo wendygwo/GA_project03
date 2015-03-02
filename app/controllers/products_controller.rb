44d3ba2cd2927bd908886c0190e6c8a2feeeb1eb
Wendy Gwo <wgwo@hotmail.com>
2015-03-02 09:46:33 -0800

    Update params allowed to be written to, in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,8 +27,6 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     # raise params.inspect
 
-    #Setting the business ID based on the hidden field passed in
-    @product.business_id= params[:product][:business_id]
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -72,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :quantity, :price, :description, :category, :product_image)
+      params.require(:product).permit(:name, :quantity, :price, :description, :category, :product_image, :business_id)
     end
 end
