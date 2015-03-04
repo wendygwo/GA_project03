893c5f2b4451ada89ef1b661f335199ce745e85f
Wendy Gwo <wgwo@hotmail.com>
2015-03-04 15:19:55 -0800

    Add geocode to business model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303062721) do
+ActiveRecord::Schema.define(version: 20150304225042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20150303062721) do
     t.string   "biz_image_content_type"
     t.integer  "biz_image_file_size"
     t.datetime "biz_image_updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "categories", force: :cascade do |t|
