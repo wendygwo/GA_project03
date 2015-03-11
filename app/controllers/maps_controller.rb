707603d74244bb382fdd436b09b5c40e3bb0d379
kcurbelo <kevin.curbelo@gmail.com>
2015-03-10 20:40:37 -0700

    Added the main map page

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,6 +5,7 @@ class MapsController < ApplicationController
   def index
   	# @response = HTTParty.get("http://localhost:3000/businesses.json")
   	@businesses = Business.all
+    @products = Product.all
   end
 
   def new
--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
-require 'yelp'
+# require 'yelp'
 
-Yelp.client.configure do |yelp|
-  yelp.consumer_key = ENV["yelp_consumer_key"]
-  yelp.consumer_secret = ENV["yelp_consumer_secret"]
-  yelp.token = ENV["yelp_token"]
-  yelp.token_secret = ENV["yelp_token_secret"]
-end
+# Yelp.client.configure do |yelp|
+#   yelp.consumer_key = ENV["yelp_consumer_key"]
+#   yelp.consumer_secret = ENV["yelp_consumer_secret"]
+#   yelp.token = ENV["yelp_token"]
+#   yelp.token_secret = ENV["yelp_token_secret"]
+# end
