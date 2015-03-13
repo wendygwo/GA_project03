8f1d5e1a0275713152079ca49446aea97158a664
kcurbelo <kevin.curbelo@gmail.com>
2015-03-13 09:51:03 -0700

    Fixed login pathing and maps radius

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,7 +5,7 @@ class MapsController < ApplicationController
   def index
   	# @response = HTTParty.get("http://localhost:3000/businesses.json")
     if params[:location].present?
-      @businesses = Business.near(params[:location], 10, :order => "distance")
+      @businesses = Business.near(params[:location], 5, :order => "distance")
     else
       @businesses = Business.all
     end
